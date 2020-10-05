be6ac6705a04f5c2d60c27999fb5684eed69c214
Adam Kollar <gasa@vipmail.hu>
2020-10-05 08:15:12 +0200

    Rewritten update logic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,6 @@
 - First release of DropboxPortableAHK client updater
 - Automatic download and refresh of the DropBox client used by **DropboxPortableAHK**
 - Requires [7-Zip](https://www.7-zip.org/) to be installed
+
+## 1.1
+- Rewritten update method to follow the DropboxPortableAHK logic
--- Update.bat
+++ Update.bat
@@ -5,7 +5,7 @@ SET PATH=%PATH%;%ZIP_PATH%
 SET "CLIENT="
 FOR /D %%F IN (.dbfiles\bin\Dropbox\Client\*.*) DO IF NOT DEFINED CLIENT SET CLIENT=%%F
 
-MKDIR .\.update
+MKDIR .update
 
 ECHO Current client version is:
 ECHO %CLIENT%
@@ -13,16 +13,16 @@ ECHO.
 
 ECHO Downloading Dropbox Offline Installer...
 
-POWERSHELL -ExecutionPolicy RemoteSigned -File .\Update.ps1
+POWERSHELL -ExecutionPolicy RemoteSigned -File Update.ps1
 SET /P NEW_CLIENT=<client_version.txt
 ECHO.
 
 IF "%NEW_CLIENT%" == "%CLIENT%" GOTO skip
 
-IF NOT [%CLIENT%] == [] RD /s /q %CLIENT% && DEL .dbfiles\bin\Dropbox\Client\Dropbox*.dll
+IF NOT [%CLIENT%] == [] RMDIR /s /q .dbfiles\bin\Dropbox\Client && MKDIR .dbfiles\bin\Dropbox\Client
 
-7z e .\.update\DropboxOfflineInstaller.exe -o.\.update Installer.exe.* | FIND "Extracting archive:"
-7z x .\.update\Installer.exe.* -o.\.update | FIND "Extracting archive:"
+7z e .update\DropboxOfflineInstaller.exe -o.update Installer.exe.* | FIND "Extracting archive:"
+7z x .update\Installer.exe.* -o.update | FIND "Extracting archive:"
 
 ROBOCOPY .update\$0 .dbfiles\bin\Dropbox\Client /NFL /NDL /NJH /NJS /NS /NC /E /R:0 /DCOPY:T
 
@@ -37,7 +37,7 @@ GOTO end
 ECHO Current client is up to date
 
 :end
-RD /s /q .\.update
+RMDIR /s /q .update
 ECHO.
 
 PAUSE
